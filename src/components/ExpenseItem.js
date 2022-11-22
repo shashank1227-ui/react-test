6fba89a9997c1af69f69d15965cfb85ed38ee012
shashank m <sm394@dxc.com>
2022-11-22 21:12:57 +0530

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,24 @@
+function ExpenseItem(props) {
+    let dates;
+    const hello = () => {
+        alert("asdasd");
+    }
+
+
+
+    return (
+        <div>
+            <h1>{JSON.stringify(props.proper)} ghjghjghjghj
+
+
+
+            for ( let i = 0 ; i < props.proper.length; i++)
+        </h1>
+            <button onClick={hello}>asdasdasd</button>
+        </div>
+    );
+};
+
+
+
+export default ExpenseItem;
